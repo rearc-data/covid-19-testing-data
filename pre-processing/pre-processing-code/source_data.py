dc6d669d5606e7d51adb22602c384a0565a949bf
Derek Wolpert <me@derekwolpert.com>
2020-04-27 11:15:12 -0400

    updated pre-processing, added new endpoints and json version of files

--- pre-processing/pre-processing-code/source_data.py
+++ pre-processing/pre-processing-code/source_data.py
@@ -1,32 +1,40 @@
 import os
-import pandas as pd
 import boto3
-
-source_dataset_url = "https://covidtracking.com/api/"
+import urllib.request
 
 def source_dataset(s3_bucket, new_s3_key):
-	states = pd.read_csv(source_dataset_url + "states.csv", header=0, index_col=None)
-	states.to_csv("/tmp/states.csv", index=False)
 
-	states_daily = pd.read_csv(source_dataset_url + "states/daily.csv", header=0, index_col=None)
-	states_daily.to_csv("/tmp/states_daily.csv", index=False)
+	source_dataset_url = 'https://covidtracking.com/api/v1/'
 
-	states_info = pd.read_csv(source_dataset_url + "states/info.csv", header=0, index_col=None)
-	states_info.to_csv("/tmp/states_info.csv", index=False)
+	api_filenames = [
+		'states/current',
+		'states/daily',
+		'states/info',
+		'us/current',
+		'us/daily',
+		'counties',
+		'urls',
+		'states/screenshots',
+		'press'
+	]
 
-	us = pd.read_csv(source_dataset_url + "us.csv", header=0, index_col=None)
-	us.to_csv("/tmp/us.csv", index=False)
+	# Download the file from `url` and save it locally:
+	for filename in api_filenames:
+		urllib.request.urlretrieve(
+			source_dataset_url + filename + '.csv', '/tmp/' + filename.replace('/', '-') + '.csv')
+		urllib.request.urlretrieve(
+			source_dataset_url + filename + '.json', '/tmp/' + filename.replace('/', '-') + '.json')
 
-	us_daily = pd.read_csv(source_dataset_url + "us/daily.csv", header=0, index_col=None)
-	us_daily.to_csv("/tmp/us_daily.csv", index=False)
-
-	counties = pd.read_csv(source_dataset_url + "counties.csv", header=0, index_col=None)
-	counties.to_csv("/tmp/counties.csv", index=False)
-
-	#uploading new s3 dataset
+	# uploading new s3 dataset
 	s3 = boto3.client('s3')
 	folder = "/tmp"
+
+	asset_list = []
 
 	for filename in os.listdir(folder):
 		print(filename)
 		s3.upload_file('/tmp/' + filename, s3_bucket, new_s3_key + filename)
+
+		asset_list.append({'Bucket': s3_bucket, 'Key': new_s3_key + filename})
+
+	return asset_list
